75f31cc5a89795b398a4ad3da779f9faaba1c2e7
Lam Phung <lampk@Lams-MacBook-Pro.local>
2021-03-10 06:16:50 +0700

    update

--- code/00_setup.R
+++ code/00_setup.R
@@ -21,6 +21,10 @@ wflow_publish(c("analysis/DTHBTN_BenhTruyenNhiem.Rmd", "analysis/DTHBTN_BenhDuon
 wflow_publish(c("analysis/DTHBTN_BenhLayQuaVecto.Rmd"), "Add DTHBTN_BenhLayQuaVecto")
 
 
+
+
+
+
 # 2020-10-29 --------------------------------------------------------------
 
 wflow_publish(c("analysis/index.Rmd"), "Add heading")
@@ -29,3 +33,32 @@ wflow_publish(c("analysis/index.Rmd"), "Add heading")
 # 2020-11-05 --------------------------------------------------------------
 
 wflow_publish(c("analysis/DTHCB_BenhDich.Rmd"), "Add DTHCB_BenhDich")
+
+
+# 2020-11-18 --------------------------------------------------------------
+
+wflow_publish(c("analysis/index.Rmd"), "Add slides")
+
+
+# 2020-11-21 --------------------------------------------------------------
+
+wflow_publish(c("analysis/index.Rmd"), "Add ga.html")
+
+
+# 2020-11-25 --------------------------------------------------------------
+
+wflow_publish(c("analysis/index.Rmd"), "Add slide SDBT - SDKH")
+
+# 2020-12-09 --------------------------------------------------------------
+
+wflow_publish(c("analysis/index.Rmd"), "Add slide DTHBTN-BTH")
+wflow_publish(c("analysis/index.Rmd"), "Add slide DTHCB-SDNQ")
+wflow_publish(c("analysis/index.Rmd"), "Update slide DTHBTN-BTH")
+
+# 2020-12-11 --------------------------------------------------------------
+
+wflow_publish(c("analysis/index.Rmd"), "Update slide DTHBTN-BV, DTHTN-BHH")
+
+# 2020-12-28 --------------------------------------------------------------
+
+wflow_publish(c("analysis/index.Rmd"), "Update slide DTHCB - Bài tập SDKH")
